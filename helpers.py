cccdbab6ca1411ba9fdd0e990b7a91dbc864b653
林漫山 <1284568390@qq.com>
2019-02-28 20:30:08 -0600

    add helperfunctions

--- helpers.py
+++ helpers.py
@@ -1,8 +1,16 @@
-
+import nltk
+PunctuationList="!#$%&()*+,-.:;<=>?@[\]^_`{'~}"
+PunctuationList+='"'
 
 def RemovePunctuation(line):
-	s="!#$%&()*+,-./:;<=>?@[\]^_`{'~}"
-	s+='"1234567890'
-	for x in s:
+	for x in PunctuationList:
 		line=line.replace(x,'')
 	return line
+
+def GetLatterWord(sen1, w1):
+	word_list = nltk.word_tokenize(sen1)
+	for i in range(0, len(word_list) - 1):
+		if w1 in word_list[i].lower():
+			return ' '.join(K for K in word_list[i+1:] )
+	return None
+
