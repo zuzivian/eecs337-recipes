99e22a1852d79de2b26fb2604429c5a2f537ecce
linmanshan <1284568390@qq.com>
2019-03-08 17:23:55 -0600

    Update IngredientsGenrator.py

--- IngredientsGenrator.py
+++ IngredientsGenrator.py
@@ -3,18 +3,26 @@ import nltk
 
 def generator(listofIngredientsDic):
 	res=''
-	resultlist = []
+	resultlist=[]
 	for ingredient in listofIngredientsDic:
 		res=''
 		if ingredient['quantity']!='NoItem':
 			res+=ingredient['quantity']
 			res+=' '
 		if ingredient['Preparation']!='NoItem':
-			res+=ingredient['quantity']
-			res+=' '
+			marker=0
+			for i in ingredient['Preparation'].split():
+				if i in ingredient['name']:
+					marker=1
+					break
+			if marker==0:
+				res+=ingredient['quantity']
+				res+=' '
 		if ingredient['measurement']!='NoItem':
 			res+=ingredient['measurement']
 			res+=' '
 		res+=ingredient['name']
 		resultlist.append(res)
 	return resultlist
+
+print(generator([{'name': 'chopped onions', 'quantity': '1 1/2', 'measurement': 'cup', 'Preparation': 'chopped '}]))
