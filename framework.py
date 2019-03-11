cae90c96bb3ff6e9f229b7ad0f53e06487d290cb
linmanshan <1284568390@qq.com>
2019-03-11 16:52:19 -0500

    Update framework.py

--- framework.py
+++ framework.py
@@ -14,9 +14,17 @@ from Mexicanstyle import *
 
 
 def GetData(url):
+	#if url already have been crawled, diretly return the crawled data
+	if os.path.exists("data/"+url.split('/')[-1]+'.txt'):
+		with open("data/"+url.split('/')[-1]+".txt",'r')as fp:
+			print('Recipe File already exists')
+			return fp.readlines()
 
-	data=AutoCrawl(url)
-	return data
+	#if is a new url crawled the data an
+	else:
+		print('New Recipe!')
+		data=AutoCrawl(url)
+		return data
 
 def GetIngredients(text):
 	"""
